2496cf44839a62da82f12c125d0366cf4f59571a
Emil1577 <emilronquillo@gmail.com>
2023-03-19 16:57:09 -0700

    heroku

--- server/server.js
+++ server/server.js
@@ -46,9 +46,9 @@ const startApolloServer = async (typeDefs, resolvers) => {
   };
 
 
-app.listen(PORT, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+// app.listen(PORT, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
   
 
   startApolloServer(typeDefs, resolvers);
