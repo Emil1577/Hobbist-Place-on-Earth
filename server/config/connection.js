bc9b53f3a5e3d1af55c5c719ce822cd8ac45af51
Emil1577 <emilronquillo@gmail.com>
2023-03-19 21:33:12 -0700

    make this work

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/hobbies',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/hobbies',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
+
+
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/hobbies',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
 
 module.exports = mongoose.connection;
