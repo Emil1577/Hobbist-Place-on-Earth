be8eb2c376aebff91df4a19884eaf864b867b83e
Emil1577 <emilronquillo@gmail.com>
2023-03-19 21:49:14 -0700

    make this work

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://emilronquillo:Emil#1577@cluster0.inf0h9w.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/hobbies',
   {
